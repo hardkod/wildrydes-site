fd5e6e51d119961973cb1f507166793367280527
mszypke <mszypke@bluesoft.com>
2024-01-10 10:22:36 +0100

    Updated config

--- public/js/config.js
+++ public/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_71MRFSUIm',
-        userPoolClientId: '9mfkc9st8coj8s6dq33temi5t',
+        userPoolId: 'eu-west-1_zDCuKyd3J',
+        userPoolClientId: '71anf933010bu4d3g4m21gu2k7',
         region: 'eu-west-1'
     },
     api: {
-        invokeUrl: 'https://etq3kshdfk.execute-api.eu-west-1.amazonaws.com/prod'
+        invokeUrl: 'https://bkspmea610.execute-api.eu-west-1.amazonaws.com/prod'
     }
 };
